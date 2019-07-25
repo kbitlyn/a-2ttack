42e284b16b3c3418a2914483bb2a5420c36b7d3e
Channon <sub.channon@gmail.com>
2019-07-25 13:39:58 -0700

    added fuction fetchJSON into js file

--- scriptsheet.js
+++ scriptsheet.js
@@ -15,4 +15,19 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function fetchJSON(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange= function() {
+    if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+            var data = JSON.parse(httpRequest.responseText);
+            if (callback) callback(data);
+        }
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+
+}
+
 window.onload = init;
