e8d3559c6c1f3a78271401026f3e0d3f7ed6242d
Channon <sub.channon@gmail.com>
2019-07-26 11:38:47 -0700

    added python files for the json

--- oftenStressedData.py
+++ oftenStressedData.py
@@ -3,15 +3,24 @@ import json
 
 # read file
 with open('oftenStressed.json', 'r') as myfile:
-    data=myfile.read()
+    stress=myfile.read()
 
 # parse file
-obj = json.loads(data)
+obj = json.loads(stress)
 
 all = 0
 sometimes = 0
 rarely = 0
 never = 0
+
+##########################
+
+other = 0
+teachers = 0
+college = 0
+relationships = 0
+friends = 0
+parents = 0
 
 for i in obj:
     if i["Event Category"] == "All the time":
@@ -23,4 +32,27 @@ for i in obj:
     elif i["Event Category"] == "Never":
         never += 1
 
+############################
+
+with open('whatStress.json', 'r') as myfile:
+    stress=myfile.read()
+
+obj = json.loads(stress)
+
+for i in obj:
+    if i["Event Category"] == "Other":
+        other += 1
+    elif i["Event Category"] == "Teachers":
+        teachers += 1
+    elif i["Event Category"] == "College":
+        college += 1
+    elif i["Event Category"] == "Relationships":
+        relationships += 1
+    elif i["Event Category"] == "Friends":
+        friends += 1
+    elif i["Event Category"] == "Parents":
+        parents += 1
+
+
 print(all)
+print(relationships)
